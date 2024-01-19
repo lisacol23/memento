bd10645fadda1d21d451cdf3e8e57b3b6c137ec3
gabrielaflorian <gabriela.florian@mail.polimi.it>
2024-01-19 17:20:58 +0100

    ordine pallini scene
    ordine pallini e una polaroid png

--- slides.js
+++ slides.js
@@ -7,67 +7,207 @@ const boxes = [
   {
     section: 1,
     left: 1,
-    right: 2,
+    right: 5,
   },
   {
     section: 2,
     left: 2,
-    right: 1,
+    right: 7,
   },
   {
     section: 3,
     left: 3,
-    right: 4,
+    right: 9,
   },
   {
     section: 4,
     left: 4,
-    right: 0,
+    right: 11,
   },
   {
     section: 5,
     left: 5,
-    right: 5,
+    right: 13,
+  },
+  {
+    section: 6,
+    left: 6,
+    right: 15,
   },
   {
     section: 7,
     left: 7,
-    right: 6,
+    right: 17,
   },
   {
     section: 8,
     left: 8,
-    right: 12,
+    right: 19,
   },
   {
     section: 9,
     left: 9,
-    right: 0,
+    right: 21
   },
   {
     section: 10,
     left: 10,
-    right: 6,
+    right: 23,
   },
   {
     section: 11,
     left: 11,
-    right: 6,
+    right: 25,
   },
   {
     section: 12,
     left: 12,
-    right: 6,
+    right: 27,
   },
   {
     section: 13,
-    left: 12,
-    right: 6,
+    left: 13,
+    right: 29,
   },
   {
     section: 14,
-    left: 12,
+    left: 14,
+    right: 31,
+  },
+  {
+    section: 15,
+    left: 15,
+    right: 33,
+  },
+  {
+    section: 16,
+    left: 16,
+    right: 35,
+  },
+  {
+    section: 17,
+    left: 17,
+    right: 37,
+  },
+  {
+    section: 18,
+    left: 18,
+    right: 39,
+  },
+  {
+    section: 19,
+    left: 19,
+    right: 41,
+  },
+  {
+    section: 20,
+    left: 20,
+    right: 43,
+  },
+  {
+    section: 21,
+    left: 21,
+    right: 45,
+  },
+  {
+    section: 22,
+    left: 22,
+    right: 44,
+  },
+  {
+    section: 23,
+    left: 23,
+    right: 42,
+  },
+  {
+    section: 24,
+    left: 24,
+    right: 40,
+  },
+  {
+    section: 25,
+    left: 25,
+    right: 38,
+  },
+ {
+    section: 26,
+    left: 26,
+    right: 36,
+  },
+  {
+    section: 27,
+    left: 27,
+    right: 34,
+  },
+  {
+    section: 28,
+    left: 28,
+    right: 32,
+  }, {
+    section: 29,
+    left: 29,
+    right: 30,
+  }, {
+    section: 30,
+    left: 30,
+    right: 28,
+  }, {
+    section: 31,
+    left: 31,
+    right: 26,
+  }, {
+    section: 32,
+    left: 32,
+    right: 24,
+  }, {
+    section: 33,
+    left: 33,
+    right: 22,
+  }, {
+    section: 34,
+    left: 34,
+    right: 20,
+  }, {
+    section: 35,
+    left: 35,
+    right: 18,
+  }, {
+    section: 36,
+    left: 36,
+    right: 15,
+  }, {
+    section: 37,
+    left: 37,
+    right: 14,
+  }, {
+    section: 38,
+    left: 38,
+    right: 12,
+  },
+  {
+    section: 39,
+    left: 39,
+    right: 10,
+  },
+  {
+    section: 40,
+    left: 40,
+    right: 8,
+  },
+  {
+    section: 41,
+    left: 41,
     right: 6,
+  },
+  {
+    section: 42,
+    left: 42,
+    right: 4,
+  },
+  {
+    section: 43,
+    left: 43,
+    right: 2,
   },
 ];
 const secciones = document.querySelectorAll('.section');
